5af05009a766f22021052cf48ab349ef72b6963d
bhupendra1011 <bhupendra1011@gmail.com>
2020-03-11 10:52:01 +0530

    adding mobile breakpints for header nav

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -10,10 +10,14 @@ import {
 	Tab,
 	Button,
 	Menu,
-	MenuItem
+	MenuItem,
+	useMediaQuery,
+	useTheme
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
+
+// for responsiveness
 
 function ElevationScroll(props) {
 	const { children } = props;
@@ -31,10 +35,22 @@ function ElevationScroll(props) {
 const useStyles = makeStyles(theme => ({
 	toolbarMargin: {
 		...theme.mixins.toolbar,
-		marginBottom: "3em"
+		marginBottom: "3em",
+		[theme.breakpoints.down("md")]: {
+			marginBottom: "2em"
+		},
+		[theme.breakpoints.down("xs")]: {
+			marginBottom: "1.75em"
+		}
 	},
 	logo: {
-		height: "7em"
+		height: "7em",
+		[theme.breakpoints.down("md")]: {
+			height: "6em"
+		},
+		[theme.breakpoints.down("xs")]: {
+			height: "5.5em"
+		}
 	},
 	tabContainer: {
 		marginLeft: "auto"
@@ -67,6 +83,9 @@ const useStyles = makeStyles(theme => ({
 
 function Header() {
 	const classes = useStyles();
+	const theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.down("md"));
+
 	const [activeTab, setActiveTab] = useState(0);
 	const handleTabChange = (e, value) => setActiveTab(value);
 
@@ -135,6 +154,65 @@ function Header() {
 				break;
 		}
 	}, [activeTab]);
+
+	const tabs = (
+		<>
+			<Tabs
+				value={activeTab}
+				className={classes.tabContainer}
+				onChange={handleTabChange}
+				indicatorColor='primary'
+			>
+				<Tab className={classes.tabs} label='Home' component={Link} to='/' />
+				<Tab
+					aria-owns={anchorEle ? "simple-menu" : undefined}
+					aria-haspopup={anchorEle ? "true" : undefined}
+					onMouseOver={e => handleClick(e)}
+					className={classes.tabs}
+					label='Services'
+					component={Link}
+					to='/services'
+				/>
+				<Tab
+					className={classes.tabs}
+					label='Contact us'
+					component={Link}
+					to='/contactus'
+				/>
+			</Tabs>
+			<Button variant='contained' color='secondary' className={classes.button}>
+				{" "}
+				Free Estimate
+			</Button>
+			<Menu
+				id='simple-menu'
+				keepMounted
+				open={open}
+				anchorEl={anchorEle}
+				onClose={handleClose}
+				MenuListProps={{ onMouseLeave: handleClose }}
+				onClick={() => setActiveTab(1)}
+				elevation={0}
+				classes={{ paper: classes.menu }}
+			>
+				{options.map((menu, index) => (
+					<MenuItem
+						classes={{ root: classes.menuItem }}
+						component={Link}
+						to={menu.to}
+						onClick={() => {
+							setSelectedIndex(index);
+							handleClose();
+						}}
+						selected={index === selectedIndex && activeTab === 1}
+					>
+						{menu.name}
+					</MenuItem>
+				))}
+			</Menu>
+		</>
+	);
+
 	return (
 		<>
 			<ElevationScroll>
@@ -143,68 +221,7 @@ function Header() {
 						<Link to='/' onClick={() => setActiveTab(0)}>
 							<img className={classes.logo} src={logo} alt='company logo' />
 						</Link>
-						<Tabs
-							value={activeTab}
-							className={classes.tabContainer}
-							onChange={handleTabChange}
-							indicatorColor='primary'
-						>
-							<Tab
-								className={classes.tabs}
-								label='Home'
-								component={Link}
-								to='/'
-							/>
-							<Tab
-								aria-owns={anchorEle ? "simple-menu" : undefined}
-								aria-haspopup={anchorEle ? "true" : undefined}
-								onMouseOver={e => handleClick(e)}
-								className={classes.tabs}
-								label='Services'
-								component={Link}
-								to='/services'
-							/>
-							<Tab
-								className={classes.tabs}
-								label='Contact us'
-								component={Link}
-								to='/contactus'
-							/>
-						</Tabs>
-						<Button
-							variant='contained'
-							color='secondary'
-							className={classes.button}
-						>
-							{" "}
-							Free Estimate
-						</Button>
-						<Menu
-							id='simple-menu'
-							keepMounted
-							open={open}
-							anchorEl={anchorEle}
-							onClose={handleClose}
-							MenuListProps={{ onMouseLeave: handleClose }}
-							onClick={() => setActiveTab(1)}
-							elevation={0}
-							classes={{ paper: classes.menu }}
-						>
-							{options.map((menu, index) => (
-								<MenuItem
-									classes={{ root: classes.menuItem }}
-									component={Link}
-									to={menu.to}
-									onClick={() => {
-										setSelectedIndex(index);
-										handleClose();
-									}}
-									selected={index === selectedIndex && activeTab === 1}
-								>
-									{menu.name}
-								</MenuItem>
-							))}
-						</Menu>
+						{matches ? "mobile content only" : tabs}
 					</Toolbar>
 				</AppBar>
 			</ElevationScroll>
